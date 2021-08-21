21eef82ccc2808a2d708f7060baa7e7155c3147e
Netsky <56271887+TheNetsky@users.noreply.github.com>
2021-08-21 16:54:16 +0200

    ManhuaPlus, update details selector
    New chapters are now loaded in a "chapter-video-frame" div, so I added this to the selector.

--- src/ManhuaPlus/ManhuaPlus.ts
+++ src/ManhuaPlus/ManhuaPlus.ts
@@ -4,7 +4,7 @@ import {Madara} from '../Madara'
 const MANHUAPLUS_DOMAIN = "https://manhuaplus.com"
 
 export const ManhuaPlusInfo: SourceInfo = {
-    version: '1.1.1',
+    version: '1.1.2',
     name: 'ManhuaPlus',
     description: 'Extension that pulls manga from manhuaplus.com',
     author: 'GameFuzzy',
@@ -24,5 +24,5 @@ export class ManhuaPlus extends Madara {
     baseUrl: string = MANHUAPLUS_DOMAIN
     languageCode: LanguageCode = LanguageCode.ENGLISH
     hasAdvancedSearchPage = true
-    chapterDetailsSelector: string = 'li.blocks-gallery-item > figure > img, div.page-break > img'
+    chapterDetailsSelector: string = 'li.blocks-gallery-item > figure > img, div.page-break > img, div#chapter-video-frame > p > img'
 }
